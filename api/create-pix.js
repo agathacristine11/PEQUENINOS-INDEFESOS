89b23b6174e2ecf2c6dfb1e806a517bab598db41
agathacristine11 <agathacristine098@proton.me>
2026-08-17 16:14:40 -0300

    Update create-pix.js

--- api/create-pix.js
+++ api/create-pix.js
@@ -17,7 +17,10 @@ import {
 } from '../lib/config.js';
 import { rateLimitCheck, clientIp } from '../lib/ratelimit.js';
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', PUBLIC_URL || '*');
